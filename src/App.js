e6d921e29520730f1a39eb95a8d760382351bd15
black-fire07 <deepeshrawat77@gmail.com>
2021-10-17 18:55:05 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,106 @@
+import React, { useEffect, useState } from "react";
+import Web3 from 'web3';
+import './App.css';
+import { ImageList, Input } from '@material-ui/core';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container'
+
+const App = () => {
+
+  let content;
+  const [val, setval] = useState("");
+
+  const [account, setAccount] = useState("");
+  const [supp, setSupp] = useState("");
+  
+  const [Hello, setHello] = useState({});
+
+  useEffect(() => {
+    loadWeb3()
+    // console.log(window.web3);
+    loadBlockdata()
+  },[]);
+
+  const loadWeb3 = async () => {
+    if(window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if(window.web3){
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else{
+      window.alert('non etherreum browser');
+    }
+  }
+  
+  const loadBlockdata = async () => {
+    const web3 = window.web3
+    const accounts = await new web3.eth.getAccounts();
+    setAccount(accounts[0]);
+    
+    const networkId =  await web3.eth.net.getId();
+    
+    if(networkId == 1) {
+      const Hell = new web3.eth.Contract([{"inputs":[{"internalType":"string","name":"_name","type":"string"},{"internalType":"string","name":"_symbol","type":"string"},{"internalType":"string","name":"_initBaseURI","type":"string"},{"internalType":"string","name":"_initNotRevealedUri","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"baseExtension","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"cost","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"maxMintAmount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"maxSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_mintAmount","type":"uint256"}],"name":"mint","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"notRevealedUri","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bool","name":"_state","type":"bool"}],"name":"pause","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"paused","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_newBaseExtension","type":"string"}],"name":"setBaseExtension","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_newBaseURI","type":"string"}],"name":"setBaseURI","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_newCost","type":"uint256"}],"name":"setCost","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_notRevealedURI","type":"string"}],"name":"setNotRevealedURI","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_newmaxMintAmount","type":"uint256"}],"name":"setmaxMintAmount","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"showTheCollection","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"showtime","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_owner","type":"address"}],"name":"walletOfOwner","outputs":[{"internalType":"uint256[]","name":"","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"payable","type":"function"}], "0x9091c4B6903ff4aB53FAB06869bD3a62467e00F4");
+      setHello(Hell);
+      const supply = await Hell.methods.totalSupply().call();
+      setSupp(supply)
+    } else {
+      window.alert('Token contract not deployed to detected network. Change it to ethereum mainnet')
+    }
+    console.log(Hello)
+  }
+
+  const mintt = async (amount) => {
+    await Hello.methods.mint(parseInt(val)).send({value: amount, from: account}).on('transactionHash', (hash) => {
+      loadBlockdata();
+    })
+  }
+  
+    return (
+
+      <div className="nft">
+    <Jumbotron fluid style={{display:"block",margin:"auto", marginTop:"13%",height:"100%"}}>
+    <Container className="text-center" >
+      <h1 style={{color:"white", fontSize:"5rem", fontWeight:"600", textShadow:"5px 5px 20px red"}}><b>DOSE OF HENTAI</b></h1>
+      <p className="text-center" style={{paddingTop: "22px",color:"#f7f972", fontFamily:"cursive", fontSize:"2rem"}}>
+      Your journey to the AR world of hentai begins
+      </p>
+      <p style={{fontSize:"1.2rem"}}>Total Minted: <strong>{supp}/6969</strong></p>
+      <p style={{fontSize:"1.2rem"}}>1 Dhentai cost <strong>0.09 eth</strong></p>
+      <p style={{fontSize:"1.2rem"}}>Max 20 nft's can be minted per transaction</p>
+      <a href="https://opensea.io/collection/dose-of-hentai"> <strong>Opensea</strong> </a>
+
+      <p>
+      
+      <Input style = {{paddingTop: "22px", marginRight:"9px", textAlign:"center"}} type="text" placeholder="Enter 1 to 20"  onChange={(event) => {
+              const tokenAmount = event.target.value.toString()
+              setval(tokenAmount)
+            }}/>
+            {/* </div> */}
+        <button type="button" class="btn text-center" onClick={(e) => {
+          if(val < 1 || val > 20){
+            alert("Enter number between 1 to 20");
+          }
+          else{
+          let etherAmount
+          etherAmount = "0.09" * val
+          etherAmount = etherAmount.toString()
+          etherAmount = window.web3.utils.toWei(etherAmount, 'Ether')
+          mintt(etherAmount);
+          console.log(etherAmount);
+          }
+      }}>
+          Mint
+        </button>
+      </p>
+     
+      </Container>
+    </Jumbotron>
+      </div>
+    );
+  }
+
+export default App;
+
